be7713caada2727cb59037f7792d2ae49e1ae6bc
congnghetienphong <77142692+congnghetienphong@users.noreply.github.com>
2021-09-03 21:31:21 +0700

    update

--- block.test.js
+++
@@ -1,9 +0,0 @@
-const Block = require('./block');
-
-describe('Block', () => {
-    let data, lastBlock, block;
-    beforeEach(() => {
-        data = 'bar';
-    lastBlock = Block.genesis();
-    block = Block.mineBlock(lastBlock, data);
-    });
